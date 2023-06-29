e0aed1bbf72656cbd7c68a31a5a59c8ba6748325
farrokhdev <farrokhdevs@gmail.com>
2023-06-29 11:40:21 +0330

    upddd

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const socketCon = new Server(server, {
   cors: {
-    origin: "https://chat-app-8hqd.onrender.com/",
+    origin: "https://chat-app-8hqd.onrender.com",
     methods: ["GET", "POST"],
   },
 });
